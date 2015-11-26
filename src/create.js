8b6fc013f379de8bc662feb185aaad810fe51917
szymon.kunowski <szymon.kunowski@gmail.com>
2015-11-26 12:07:43 +0100

    Add default controller and index.html

--- src/create.js
+++ src/create.js
@@ -2,14 +2,32 @@ var fs = require('fs-extra');
 
 var create = function(template,args) {
     if(checkArgs(template.require,args)) {
-        fs.readFile(template.template, function (err, data) {
-            if (err) throw err;
-            for (search in args) {
-
+        fs.readFile(template.template, "UTF-8", function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                var search;
+                for (search in args) {
+                    var re = new RegExp("{{" + search + "}}", 'g');
+                    data = data.replace(re, args[search]);
+                }
+                var fileName = (template.fileName) ? template.fileName : args.name;
+                fs.outputFile(args.dir + template.outputDir + fileName + template.type + template.ext, data, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("The file was saved!");
+                });
             }
-            console.log(data);
+
         });
+    } else {
+        console.log('Wrong args');
     }
+};
+
+var checkArgs = function(require, args) {
+return true;
 };
 
 module.exports = {
